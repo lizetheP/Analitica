6b02c54cd8310a393ff1945bc1de9197e3c78357
Lizethe <lizetheperez@gmail.com>
2022-08-31 14:49:55 -0500

    subir

--- CalendarioAgo2022/presentaciones/claseExpresiones.py
+++ CalendarioAgo2022/presentaciones/claseExpresiones.py
@@ -1,14 +1,19 @@
-Laura Sanchez
+import re
+
+texto = """Laura Sánchez
 442 153-32-42
 lasanch@hotmail.com
 http://www.laurasan.com
 
-Pedro Lopez
+Pedro López
 448 342 33 12
 pedrolo54@gmail.com
 https://www.pedro.lopez.com.mx
 
-Violeta Perez
+Violeta Pérez
 443 214 43 72
 violeta45@outlook.com
 violeta.net
+"""
+
+print(re.findall("[a-zA-Z]", texto, flags=re.M ))

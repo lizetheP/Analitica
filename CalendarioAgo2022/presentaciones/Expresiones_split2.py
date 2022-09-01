2968bc28116c72e49de976246acbaee749ee2848
Lizethe <lizetheperez@gmail.com>
2022-09-01 14:55:44 -0500

    SUBIR

---
+++ CalendarioAgo2022/presentaciones/Expresiones_split2.py
@@ -0,0 +1,15 @@
+import re
+
+texto = """Podrá nublarse el sol eternamente; 
+Podrá secarse en un instante el mar; 
+Podrá romperse el eje de la tierra 
+como un débil cristal. 
+¡todo sucederá! Podrá la muerte 
+cubrirme con su fúnebre crespón; 
+Pero jamás en mí podrá apagarse 
+la llama de tu amor."""
+
+# Dividir el texto por caracteres que no son alfanuméricos
+
+lista = re.split("\n", texto)
+print(lista)
